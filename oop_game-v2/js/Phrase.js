574776f5bb4ca0cf056830421b227e356eb0b81d
Akhaled19 <akhaled@clarku.edu>
2019-12-09 18:59:15 -0500

    added the backbone structure to the phrase.js

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -1,15 +1,29 @@
 //Global Variables 
 
 //Phrase class 
-
+class Phrase {
     //add a constructor, and pass in 'phrase' parameter
+    constructor(phrase) {
         //initialize a 'phrase' property set to the phrase 
+        this.phrase = phrase;
+    }    
+    //add a method and call it 'addPhraseToDisplay()'
+    addPhraseToDisplay() {
+        //this method adds the phrase to the gameboard
+        let gameboard = this.phrase;
+    }    
+    //add a second method and call it 'checkLetter()' 
+    checkLetter() {
+        key = document.getElementsByClassName('key');
+        let i = key;
+        //this method checks if a letter is in the phrase
+        if(this.phrase.includes(i)) {
+            //do something
+        }
+    }        
+    // add a third method and call it 'showMatchedLetter()'
+    showMatchedLetter() {
+        //this method reveals the letter(s) on the board that matches the player's section
 
-        //add a method and call it 'addPhraseToDisplay()'
-            //this method adds the phrase to the gameboard
-
-        //add a second method and call it 'checkLetter()' 
-            //this method checks if a letter us in the phrase
-            
-        // add a third method and call it 'showMatchedLetter()'
-            //this method reveals the letter(s) on the board that matches the player's section
+    }    
+}
